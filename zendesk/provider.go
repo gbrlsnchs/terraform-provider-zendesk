0bf3df320f6c84321eeed97b2f0af8483e39f2b2
970uraj <utkarsh.raj@sonova.com>
2024-03-20 00:17:13 +0100

    added view terraform

--- zendesk/provider.go
+++ zendesk/provider.go
@@ -50,6 +50,7 @@ func Provider() *schema.Provider {
 			"zendesk_group":        resourceZendeskGroup(),
 			"zendesk_ticket_field": resourceZendeskTicketField(),
 			"zendesk_macro": resourceZendeskMacro(),
+			"zendesk_view": resourceZendeskView(),
 			"zendesk_user_field":   resourceZendeskUserField(),
 			"zendesk_ticket_form":  resourceZendeskTicketForm(),
 			"zendesk_trigger":      resourceZendeskTrigger(),
